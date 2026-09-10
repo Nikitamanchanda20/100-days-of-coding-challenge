70485acf9e5689442ce8a1f70e924547d23b3ef5
Nikitamanchanda20 <manchandanikita47@gmail.com>
2026-09-10 19:53:22 +0530

    Add files via upload

---
+++ day24/day24ques1.c
@@ -0,0 +1,23 @@
+//Write a program to print the following pattern:
+// *
+// **
+// ***
+// ****
+// *****
+
+#include <stdio.h>
+int main()
+{
+int j,i;
+char a;
+a='*';
+for (int i=1;i<=5;i++)
+{
+for (int j=1;j<=i;j++)
+{
+    printf("%c",a);
+}
+printf("\n");
+}
+return 0;
+}
