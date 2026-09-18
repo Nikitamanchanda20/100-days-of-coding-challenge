4343c239b6df7a0b20d4b52b968ca8d469d8c38e
Nikitamanchanda20 <manchandanikita47@gmail.com>
2026-09-18 19:42:59 +0530

    Add files via upload

---
+++ day 40/day40ques1.c
@@ -0,0 +1,21 @@
+//Perform diagonal traversal of a matrix.
+#include <stdio.h>
+
+int main() {
+    int a[3][3] ={
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9},
+    };
+
+    int i, j;
+
+    for(i = 0; i < 3; i++) {
+        for(j = 0; j < 3; j++) {
+            if(i == j)
+                printf("%d ", a[i][j]);
+        }
+    }
+
+    return 0;
+}
