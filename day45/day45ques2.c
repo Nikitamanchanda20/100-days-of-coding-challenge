56a2f2be4ba42ceba59f6b01a311727f624a0bb8
Nikitamanchanda20 <manchandanikita47@gmail.com>
2026-09-23 20:31:16 +0530

    Add files via upload

---
+++ day45/day45ques2.c
@@ -0,0 +1,20 @@
+//Toggle case of each character in a string.
+#include <stdio.h>
+int main()
+{
+char str[100];
+int i;
+   printf("Enter string:");
+   scanf("%s",str);
+ 
+for(i=0;str[i] !='\0';i++)
+{
+ if(str[i]>='a' && str[i]<='z')
+     str[i]=str[i]-32;
+
+else if(str[i]>='A' && str[i]<='Z')
+        str[i]=str[i]+32;
+}
+printf("Toggle case:%s",str);
+return 0;
+}
